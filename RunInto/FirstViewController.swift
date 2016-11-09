6c69d4aa6a997d6037a4de85ea19093af448f934
YongYang <yyongdl@cn.ibm.com>
2016-11-09 11:25:56 +0800

    add code

---
+++ RunInto/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RunInto
+//
+//  Created by YongYang on 09/11/2016.
+//  Copyright © 2016 YongYang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
